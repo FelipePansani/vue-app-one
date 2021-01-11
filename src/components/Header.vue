7d884f58a758318b59681b452a72a945236a3e06
Felipe Pansani <felipe_pansani@outlook.com>
2021-01-11 13:38:34 -0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <h1>TodoList</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: ["header"],
+};
+</script>
+
+<style scoped>
+.header {
+  background: #333;
+  color: #fff;
+  padding: 10px;
+}
+.header a {
+  color: #fff;
+  padding-right: 5px;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="todo-item" v-bind:class="{ 'is-complete': todo.completed }">
+    <p>
+      <input type="checkbox" v-on:change="markComplete">{{todo.title}}
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"],
+    methods: {
+        markComplete() {
+            this.todo.completed = !this.todo.completed;
+        }
+    }
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #e7e7e7;
+  padding: 10px;
+  border-bottom: 1px solid black;
+}
+
+.is-complete {
+  text-decoration: line-through;
+}
+
+.del {
+  background: rgb(238, 79, 79);
+  color: white;
+  padding: 5px 9px;
+  border-radius: 50%;
+  border: none;
+  float: right;
+  cursor: pointer;
+}
+</style>
